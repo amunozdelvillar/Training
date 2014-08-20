3ddf448f2794b432c556660e5df9644a96d8a780
Angel Munoz <amunoz@nearsoft.com>
2014-08-19 17:23:59 -0700

    Transition done/undone todos

--- Ember/GettingStarted/js/router.js
+++ Ember/GettingStarted/js/router.js
@@ -2,11 +2,25 @@
  * Created by angel on 18/08/14.
  */
 App.Router.map(function(){
-    this.resource('todos',{path: '/'});
+    this.resource('todos',{path: '/'}, function(){
+        // add aditional child routes
+        this.route('active');
+    });
 });
 
 App.TodosRoute = Ember.Route.extend({
     model: function(){
         return this.store.find('todo');
+    }
+});
+
+App.TodosActiveRoute = Ember.Route.extend({
+    model: function(){
+        return this.store.filter('todo', function(todo){
+            return !todo.get('isCompleted');
+        });
+    },
+    renderTemplate: function(controller){
+        this.render('todos/index', {controller: controller});
     }
 });
