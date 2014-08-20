9788280b02ccb82441fb1eef5a757e164ccbe4d7
Angel Munoz <amunoz@nearsoft.com>
2014-08-20 00:20:40 -0700

    todo app complete

--- Ember/GettingStarted/js/app.js
+++ Ember/GettingStarted/js/app.js
@@ -1,3 +1,6 @@
 window.App = Ember.Application.create();
 
-App.ApplicationAdapter = DS.FixtureAdapter.extend();
+App.ApplicationAdapter = DS.LSAdapter.extend({
+    namespace: 'todos-emberjs'
+});
+//App.ApplicationAdapter = DS.FixtureAdapter.extend();
--- Ember/GettingStarted/js/controllers/todos_controller.js
+++ Ember/GettingStarted/js/controllers/todos_controller.js
@@ -29,7 +29,27 @@ App.TodosController = Ember.ArrayController.extend(
         inflection: function(){
             var remaining = this.get('remaining');
             return remaining === 1 ? 'item' : 'items';
-        }.property('remaining')
+        }.property('remaining'),
+        clearCompleted: function(){
+            var completed = this.filterBy('isCompleted', true);
+            completed.invoke('deleteRecord');
+            completed.invoke('save');
+        },
+        hasCompleted: function(){
+            return this.get('completed') > 0;
+        }.property('completed'),
+        completed: function(){
+            return this.filterBy('isCompleted', true).get('length');
+        }.property('@each.isCompleted'),
+        allAreDone: function(key, value){
+            if(value === undefined) {
+                return !!this.get('length') && this.isEvery('isCompleted');
+            } else {
+                this.setEach('isCompleted', value);
+                this.invoke('save');
+                return value;
+            }
+        }.property('@each.isCompleted')
     }
 );
 
